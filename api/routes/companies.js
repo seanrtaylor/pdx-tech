da1853c706aafedb060823a718199dc03c146702
Matt Page <matt.page@jivesoftware.com>
2017-05-11 16:18:10 -0700

    api - companies now have unique ids, company list is sorted by ids. api crud methods are all implemented and all have tests

--- api/routes/companies.js
+++ api/routes/companies.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const _ = require('lodash');
 const router = express.Router();
+const shortId = require('shortid');
+//const fs = require('fs');
 
-const companies = require('../data/companies.json');
-companies.sort((a, b) => {
-  return a.name.localeCompare(b.name);
+const jsonFile = '../data/companies.json';
+
+//in memory array of companies
+const companies = require(jsonFile);
+
+/*
+// generate an id for each company
+companies.forEach((company) => {
+  company.id = shortId.generate();
 });
+
+// sort by ids
+companies.sort((a, b) => {
+  return a.id.localeCompare(b.id);
+});
+
+//write the json back to file
+fs.writeFile(__dirname + '/' + jsonFile, JSON.stringify(companies, undefined, 2), 'utf8');
+*/
+
 
 // GET companies list
 router.get('/', function(req, res) {
@@ -14,29 +32,47 @@ router.get('/', function(req, res) {
 
 // POST create a new company
 router.post('/', function(req, res) {
-  //TODO - NOT IMPLEMENTED
-  res.status(501).end();
+  const newCompany = _.assign({ name: '', notes: '', url: ''}, req.body);
+  newCompany.id = shortId.generate();
+  companies.push(newCompany);
+  return res.status(200).json({ id: newCompany.id });
 });
 
 // GET a specific company
 router.get('/:id', function(req, res) {
-  const index = parseInt(req.params.id, 10);
-  if (_.isNumber(index) && index < companies.length){
-    return res.status(200).json(companies[index]);
+  const match = _.find(companies, { id: req.params.id });
+  if (match){
+    return res.status(200).json(match);
   }
   return res.status(404).end();
 });
 
 // PUT update a specific company
 router.put('/:id', function(req, res) {
-  //TODO - NOT IMPLEMENTED
-  res.status(501).end();
+  const match = _.find(companies, { id: req.params.id });
+  if (match){
+
+    //Update the match in memory
+    //NOTE: these aren't persisted to disk
+    ['name', 'notes', 'url'].forEach( (key) => {
+      if (key in req.body){
+        match[key] = req.body[key];
+      }
+    });
+
+    return res.status(200).end();
+  }
+  return res.status(404).end();
 });
 
 // DELETE a specific company
 router.delete('/:id', function(req, res) {
-  //TODO - NOT IMPLEMENTED
-  res.status(501).end();
+  const index = _.findIndex(companies, { id: req.params.id });
+  if (index > -1){
+    companies.splice(index);
+    return res.status(200).json({ id: req.params.id });
+  }
+  return res.status(404).end();
 });
 
 module.exports = router;
--- api/tests/index.js
+++ api/tests/index.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 const chai = require('chai');
 const _ = require('lodash');
 const expect = chai.expect;
+const jsonFile = '../data/companies.json';
+const companies = require(jsonFile);
 
 describe('api', function(){
   let app;
@@ -31,23 +33,86 @@ describe('api', function(){
         .expect('Content-Type', /json/)
         .expect(200)
         .expect(function(res){
-          expect(_.isArray(res.body));
+          expect(_.isArray(res.body)).to.be.true;
           expect(res.body.length).to.be.gt(0);
         }).end(done);
     });
-    it('POST should create a new company');
+
+    it('POST should create a new company', function(done){
+      const company = {
+        name: 'foo',
+        notes: '',
+        url: 'http://test.com'
+      };
+
+      request(app)
+        .post('/companies')
+        .send(company)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200).then(function(res){
+          company.id = res.body.id;
+          request(app)
+            .get('/companies/' + res.body.id)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function(res2){
+              expect(_.isPlainObject(res2.body)).to.be.true;
+              expect(res2.body).to.eql(company);
+            }).end(done);
+        });
+
+    });
+
   });
 
   describe('/companies/:id', function() {
     it('GET should respond with a company', function(done) {
       request(app)
-        .get('/companies')
+        .get('/companies/' + companies[0].id)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200, done);
+        .expect(200)
+        .expect(function(res){
+          expect(_.isPlainObject(res.body)).to.be.true;
+          expect(res.body).to.eql(companies[0]);
+        }).end(done);
     });
-    it('POST should create a new company');
-    it('PUT should update a new company');
-    it('DEL should delete a new company');
+
+
+    it('PUT should update a company', function(done) {
+      const company = _.clone(companies[0]);
+      company.name = 'foo';
+
+      request(app)
+        .put('/companies/' + company.id)
+        .send(_.pick(company, 'name'))
+        .set('Accept', 'application/json')
+        .expect(200).then(function(){
+          request(app)
+            .get('/companies/' + company.id)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function(res){
+              expect(_.isPlainObject(res.body)).to.be.true;
+              expect(res.body).to.eql(company);
+            }).end(done);
+        });
+    });
+
+    it('DEL should delete a new company', function(done){
+      const id = companies[0].id;
+      request(app)
+        .del('/companies/' + id)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res){
+          expect(_.isPlainObject(res.body)).to.be.true;
+          expect(res.body.id).to.eql(id);
+        }).end(done);
+    });
   });
 });
