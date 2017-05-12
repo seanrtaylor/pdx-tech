88289882786683d95dbb554a87d62a008aede975
Matthew Page <matthewnpage@gmail.com>
2017-05-11 19:01:34 -0700

    companies url constant

--- src/constants/companies.js
+++ src/constants/companies.js
@@ -2,3 +2,5 @@ export const GET_COMPANIES = 'GET_COMPANIES';
 export const GET_COMPANY = 'GET_COMPANY';
 export const UPDATE_COMPANY = 'UPDATE_COMPANY';
 export const DELETE_COMPANY = 'DELETE_COMPANY';
+
+export const COMPANIES_URL = '/companies';
